0cb7de867308a2741b3ef4ab30bbe2cb7b3e2063
reSLANE <sml37s@yahoo.com>
2018-02-03 03:10:59 -0800

    Camera's gotta go fast!

--- Assets/2D/Scripts/CameraFollow.cs
+++ Assets/2D/Scripts/CameraFollow.cs
@@ -12,6 +12,7 @@ namespace UnityStandardAssets._2D
         public float ySmooth = 8f; // How smoothly the camera catches up with it's target movement in the y axis.
         public Vector2 maxXAndY; // The maximum x and y coordinates the camera can have.
         public Vector2 minXAndY; // The minimum x and y coordinates the camera can have.
+        public float cSpeed = 1f; // Camera Speed
 
         private Transform m_Player; // Reference to the player's transform.
 
@@ -53,14 +54,14 @@ namespace UnityStandardAssets._2D
             if (CheckXMargin())
             {
                 // ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
-                targetX = Mathf.Lerp(transform.position.x, m_Player.position.x, xSmooth*Time.deltaTime);
+                targetX = Mathf.Lerp(transform.position.x, m_Player.position.x, xSmooth*Time.deltaTime*cSpeed);
             }
 
             // If the player has moved beyond the y margin...
             if (CheckYMargin())
             {
                 // ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
-                targetY = Mathf.Lerp(transform.position.y, m_Player.position.y, ySmooth*Time.deltaTime);
+                targetY = Mathf.Lerp(transform.position.y, m_Player.position.y, ySmooth*Time.deltaTime*cSpeed);
             }
 
             // The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
