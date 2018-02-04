4bf42229f6fc0c360cbc0911725ad557db38bf30
reSLANE <sml37s@yahoo.com>
2018-02-04 11:55:28 -0800

    Final Push

---
+++ Assets/spawnText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnText : MonoBehaviour {
+
+    public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            text.transform.position = new Vector2(-325.81f, -125.6f);
+        }
+    }
+}
