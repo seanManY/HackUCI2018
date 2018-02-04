61dbf3fb2711faa20a2065434cb2f782a4cd2826
smaguire <maguire.sean037@gmail.com>
2018-02-04 01:37:14 -0800

    PIUSS THAT SIHIT

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject brawn;
+    public GameObject brain;
+
+    public int brainCount;
+    public int brawnCount;
+        
+
+	// Use this for initialization
+	void Start ()
+    {
+        brainCount = 0;
+        brawnCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
