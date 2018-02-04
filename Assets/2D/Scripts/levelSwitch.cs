3d2ab61b3a3dd8305400525d38d5672e20a5caed
reSLANE <sml37s@yahoo.com>
2018-02-04 07:14:01 -0800

    the shekills

--- Assets/2D/Scripts/levelSwitch.cs
+++ Assets/2D/Scripts/levelSwitch.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class levelSwitch : MonoBehaviour {
 
+    public GameManager manag;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        manag = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+
+            
+            if (SceneManager.GetActiveScene().name[0] == 'B')
+            {
+                Debug.Log("dis som bull shit");
+                SceneManager.LoadScene("Hub");
+            }
+
+            else if(this.gameObject.transform.position.x < 0)
+            {
+                manag.brainInc();
+                int brain = manag.brainLevel();
+                SceneManager.LoadScene("Brain " + brain);
+            }
+
+            else if (this.gameObject.transform.position.x > 0)
+            {
+                manag.brawnInc();
+                int brawn = manag.brawnLevel();
+                SceneManager.LoadScene("Brawn " + brawn);
+            }
+        }
+    }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -12,14 +12,41 @@ public class GameManager : MonoBehaviour {
         
 
 	// Use this for initialization
-	void Start ()
+	void Awake ()
     {
+        DontDestroyOnLoad(transform.gameObject);
         brainCount = 0;
         brawnCount = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Start()
+    {
+        brain = GameObject.Find("Brain");
+        brawn = GameObject.Find("Brawn");
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
+
+    public void brainInc()
+    {
+        brainCount++;
+    }
+
+    public void brawnInc()
+    {
+        brawnCount++;
+    }
+
+    public int brainLevel()
+    {
+        return brainCount;
+    }
+
+    public int brawnLevel()
+    {
+        return brawnCount;
+    }
 }
